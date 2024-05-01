888ff2e12065cd9d6734a2f02e99fbeb4ee17e77
LuchoSantini <lucho_santini7@outlook.es>
2024-05-01 16:14:31 -0300

    Added component to test Products Endpoints

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import ApiCalls from "./components/ApiCalls/ApiCalls";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<ApiCalls/>} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>

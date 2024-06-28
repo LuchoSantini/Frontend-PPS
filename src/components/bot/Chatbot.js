067e6f6196835cf202be3c8bf11689e274dc27bf
IsTincho <segninitincho@gmail.com>
2024-06-28 11:19:32 -0300

    fix bug chatbot con observer

--- src/components/bot/Chatbot.js
+++ src/components/bot/Chatbot.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
 import CloseIcon from "@mui/icons-material/Close";
 import "./Chatbot.css";
 
 const theme = {
-  background: "#333333", // Fondo oscuro
+  background: "#333333",
   fontFamily: "Helvetica Neue",
-  headerBgColor: "#ff4d4f", // Rojo para el encabezado
+  headerBgColor: "#ff4d4f",
   headerFontColor: "#fff",
   headerFontSize: "15px",
-  botBubbleColor: "#ff4d4f", // Rojo para los globos del bot
+  botBubbleColor: "#ff4d4f",
   botFontColor: "#fff",
-  userBubbleColor: "#444444", // Gris oscuro para los globos del usuario
+  userBubbleColor: "#444444",
   userFontColor: "#fff",
 };
 
@@ -28,7 +28,7 @@ const steps = [
       { value: 1, label: "¿Qué productos ofrecen?", trigger: "3" },
       { value: 2, label: "¿Cómo realizar un pago?", trigger: "4" },
       { value: 3, label: "¿Cómo funciona MercadoPago?", trigger: "5" },
-      { value: 4, label: "¿Quíenes son tus creadores?", trigger: "6" },
+      { value: 4, label: "¿Quiénes son tus creadores?", trigger: "6" },
     ],
   },
   {
@@ -64,6 +64,30 @@ const steps = [
 
 const Chatbot = ({ onClose }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    setInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          setIsOpen(true);
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   const toggleChat = () => {
     setIsOpen(!isOpen);
@@ -74,15 +98,17 @@ const Chatbot = ({ onClose }) => {
       <button className="close-button" onClick={toggleChat}>
         <CloseIcon />
       </button>
-      <ThemeProvider theme={theme}>
-        <ChatBot
-          steps={steps}
-          handleEnd={({ steps, values }) => {
-            console.log(steps, values);
-          }}
-          recognitionEnable={true}
-        />
-      </ThemeProvider>
+      {initialized && (
+        <ThemeProvider theme={theme}>
+          <ChatBot
+            steps={steps}
+            handleEnd={({ steps, values }) => {
+              console.log(steps, values);
+            }}
+            recognitionEnable={true}
+          />
+        </ThemeProvider>
+      )}
     </div>
   );
 };
